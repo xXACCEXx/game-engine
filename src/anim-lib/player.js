26497d4020231eb4144e783cc07e5117286da861
Lawrence Tranter <lawrence.tranter@gmail.com>
2018-04-19 21:21:02 +1000

    complete fixes for player gif

--- src/anim-lib/player.js
+++ src/anim-lib/player.js
@@ -6,44 +6,113 @@ var size = {
 };
 
 module.exports = {
+	default: 'walk.right',
+
+	//	walking sequences
+
 	'walk.down': new Sequence([
-		{ x: 481, y: 387 },
-		{ x: 497, y: 387 },
-		{ x: 511, y: 387 }
-	], size),
+		{
+			pos: { x: 496, y: 387 },
+			size: { x: 15, y: 19 },
+			offset: { x: 7, y: 18 }
+		},
+		{
+			pos: { x: 481, y: 387 },
+			size: { x: 14, y: 19 },
+			offset: { x: 7, y: 18 }
+		},
+		{
+			pos: { x: 512, y: 387 },
+			size: { x: 15, y: 19 },
+			offset: { x: 8, y: 18 }
+		}
+	]),
 
 	'walk.left': new Sequence([
-		{ x: 527, y: 387 },
-		{ x: 545, y: 387 },
-		{ x: 550, y: 387 }
-	], size),
+		{
+			pos: { x: 560, y: 387 },
+			size: { x: 14, y: 19 },
+			offset: { x: 8, y: 18 }
+		},
+		{
+			pos: { x: 544, y: 387 },
+			size: { x: 14, y: 19 },
+			offset: { x: 8, y: 18 }
+		},
+		{
+			pos: { x: 528, y: 387 },
+			size: { x: 14, y: 19 },
+			offset: { x: 8, y: 18 }
+		}
+	]),
 
 	'walk.up': new Sequence([
-		{ x: 578, y: 387 },
-		{ x: 593, y: 387 },
-		{ x: 609, y: 387 }
-	], size),
+		{
+			pos: { x: 593, y: 387 },
+			size: { x: 14, y: 19 },
+			offset: { x: 7, y: 18 }
+		},
+		{
+			pos: { x: 578, y: 387 },
+			size: { x: 14, y: 19 },
+			offset: { x: 6, y: 18 }
+		},
+		{
+			pos: { x: 609, y: 387 },
+			size: { x: 14, y: 19 },
+			offset: { x: 6, y: 18 }
+		}
+	]),
 
 	'walk.right': new Sequence([
-		{ x: 624, y: 387 },
-		{ x: 640, y: 387 },
-		{ x: 655, y: 387 }
-	], size),
+		{
+			pos: { x: 657, y: 388 },
+			size: { x: 14, y: 19 },
+			offset: { x: 6, y: 18 }
+		},
+		{
+			pos: { x: 641, y: 388 },
+			size: { x: 14, y: 19 },
+			offset: { x: 6, y: 18 }
+		},
+		{
+			pos: { x: 625, y: 387 },
+			size: { x: 14, y: 19 },
+			offset: { x: 6, y: 18 }
+		}
+	]),
 
+	//	idle sequences
 
 	'idle.down': new Sequence([
-		{ x: 481, y: 387 }
-	], size),
+		{
+			pos: { x: 481, y: 387 },
+			size: { x: 14, y: 19 },
+			offset: { x: 7, y: 18 }
+		}
+	]),
 
 	'idle.left': new Sequence([
-		{ x: 527, y: 387 }
-	], size),
+		{
+			pos: { x: 527, y: 387 },
+			size: { x: 15, y: 19 },
+			offset: { x: 9, y: 18 }
+		}
+	]),
 
 	'idle.up': new Sequence([
-		{ x: 578, y: 387 }
-	], size),
+		{
+			pos: { x: 578, y: 387 },
+			size: { x: 14, y: 19 },
+			offset: { x: 6, y: 18 }
+		}
+	]),
 
 	'idle.right': new Sequence([
-		{ x: 624, y: 387 }
-	], size)
+		{
+			pos: { x: 625, y: 387 },
+			size: { x: 14, y: 19 },
+			offset: { x: 6, y: 18 }
+		}
+	])
 }
